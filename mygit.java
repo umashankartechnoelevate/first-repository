b49f26cd3e0bfc274bfd7fdc8ab0f1907db4a23e
Jabsons <umashankarjabagond@gmail.com>
2022-05-13 12:56:19 +0530

    secong commit

--- mygit.java
+++ mygit.java
@@ -27,3 +27,21 @@ class PrimeNumber {
 
 	}
 }
+
+
+// without user valuess
+for (int i = 20; i <= 100; i++) {
+	boolean isPrime = true;
+	for (int j = 2; j < i; j++) {
+		if (i % j == 0) {
+			isPrime = false;
+			break;
+		}
+	}
+
+	if (isPrime) {
+		System.out.println(i);
+	}
+}
+
+
